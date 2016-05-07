d83aeb9cb7dad293612d897068a527425fb3e47c
neelap <neela_reddy@outlook.com>
2016-05-07 13:23:27 +0530

    gateway

---
+++ src/main/java/com/nems/ms/gateway/NemsGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.nems.ms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class NemsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemsGatewayApplication.class, args);
+	}
+}
